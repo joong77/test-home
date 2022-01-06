343cb79fc7c8ffc63cb3d9604094f36e1c9f1e3b
leeCoder <joongholee1008@gmail.com>
2022-01-06 16:21:55 +0900

    test1 version

--- web.js
+++ web.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
 const PORT = 8001
-app.set('views', __dirname + '/views');
+
+app.set('views', './src/views');
 app.set('view engine', 'ejs')
+
+//router
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+//src setting
+app.use(express.static(`${__dirname}/src/public`));
+
+
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`)
 })
-// web.js
+
+
+module.exports = app;
